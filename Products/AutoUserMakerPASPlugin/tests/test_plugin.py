a540c53828ccda6567990b3e98875127982d6d97
tom_gross <tom_gross@localhost>
2010-10-22 07:31:26 +0000

    added plugin-tests
    svn path=/Products.AutoUserMakerPASPlugin/trunk/; revision=146740

---
+++ Products/AutoUserMakerPASPlugin/tests/test_plugin.py
@@ -0,0 +1,53 @@
+import unittest
+
+from Products.AutoUserMakerPASPlugin.tests.base import PluginTestCase
+from Products.AutoUserMakerPASPlugin.Extensions.Install import \
+    addautousermakerplugin
+
+
+class AutoUserMakerPASPluginTests(PluginTestCase):
+
+    def afterSetUp(self):
+        self.plugin = addautousermakerplugin(self.portal.acl_users)
+
+    def test_authentication(self):
+        auth = self.plugin.authenticateCredentials
+        self.assertFalse(auth({}))
+        self.assertEqual(auth({'user_id': 'foobar'}), ('foobar', 'foobar'))
+
+    def test_challenge(self):
+        class DummyReq(object):
+
+            def __init__(self, url):
+                self.ACTUAL_URL = url
+
+        class DummyResp(object):
+            url = ''
+
+            def redirect(self, url, lock=True):
+                self.url = url
+
+        request = DummyReq('http://www.example.org/')
+        response = DummyResp()
+        self.assertFalse(response.url)
+        self.plugin.challenge(request, response)
+        self.assertEqual(response.url, 'https://www.example.org/')
+
+    def test_loginurl(self):
+        self.assertEqual(
+            self.plugin.loginUrl('http://www.example.org/path?q=hello+world'),
+            'https://www.example.org/path?q=hello+world')
+        self.assertEqual(
+            self.plugin.loginUrl('https://www.example.org/https/stays'), '')
+        self.assertEqual(
+            self.plugin.loginUrl('ftp://ftp.example.org/path'), '')
+
+
+def test_suite():
+    """ This is the unittest suite """
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(AutoUserMakerPASPluginTests))
+    return suite
+
+# EOF
