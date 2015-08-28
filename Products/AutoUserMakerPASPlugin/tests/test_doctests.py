d4f0da9420940a86070c85054f7db552e1c14193
Tom Gross <itconsense@gmail.com>
2015-08-28 13:57:49 +0000

    ported to plone.app.testing

---
+++ Products/AutoUserMakerPASPlugin/tests/test_doctests.py
@@ -0,0 +1,32 @@
+""" Test docstrings
+"""
+
+__revision__ = '0.1'
+
+import unittest
+import doctest
+import glob
+import os
+
+from plone.testing import layered
+
+from Products.AutoUserMakerPASPlugin.tests.base import AUTOUSERMAKERPASPLUGIN_FUNCTIONAL_TESTING
+
+
+def listDoctests():
+    home = os.path.dirname(__file__)
+    return [ii for ii in glob.glob(os.path.sep.join([home, '*.txt']))]
+
+
+OPTIONFLAGS = doctest.REPORT_ONLY_FIRST_FAILURE|doctest.ELLIPSIS
+
+
+def test_suite():
+    tests = [layered(doctest.DocTestSuite('Products.AutoUserMakerPASPlugin.auth',
+                                  optionflags=OPTIONFLAGS),
+                     layer=AUTOUSERMAKERPASPLUGIN_FUNCTIONAL_TESTING),]
+    tests.extend([layered(doctest.DocFileSuite(os.path.basename(filename),
+                                        optionflags=OPTIONFLAGS),
+                          layer=AUTOUSERMAKERPASPLUGIN_FUNCTIONAL_TESTING)
+             for filename in listDoctests()])
+    return unittest.TestSuite(tests)
